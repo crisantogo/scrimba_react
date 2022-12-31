44c2d8d5f9f2a339956cb10c8b14ade62c91abfc
crisantogo <christopher.gopeesingh@gmail.com>
2022-12-31 16:10:04 -0400

    Styled Navbar

--- react_fun_facts/react_fun_facts/src/components/Navbar.js
+++ react_fun_facts/react_fun_facts/src/components/Navbar.js
@@ -4,6 +4,8 @@ function Navbar(){
     return(
         <nav>
             <img src={logo} alt="react logo" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
         </nav>
     )
 }
